fc47522a300c3c695c2743e843af05907987ebdb
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-26 19:34:49 +0200

    initial version

---
+++ src/main/java/at/hochbichler/microservices/udemycurrencyexchangeservice/UdemyCurrencyExchangeServiceApplication.java
@@ -0,0 +1,12 @@
+package at.hochbichler.microservices.udemycurrencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCurrencyExchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyCurrencyExchangeServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/at/hochbichler/microservices/udemycurrencyexchangeservice/UdemyCurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package at.hochbichler.microservices.udemycurrencyexchangeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyCurrencyExchangeServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
