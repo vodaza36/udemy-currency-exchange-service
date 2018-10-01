b0cae6c7042a9a0ab4299a6cfa4d2693d62971be
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-01 18:21:41 +0200

    add sleuth

--- src/main/java/at/hochbichler/microservices/udemycurrencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/at/hochbichler/microservices/udemycurrencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package at.hochbichler.microservices.udemycurrencyexchangeservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private Environment environment;
 
@@ -21,6 +25,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }
--- src/main/java/at/hochbichler/microservices/udemycurrencyexchangeservice/UdemyCurrencyExchangeServiceApplication.java
+++ src/main/java/at/hochbichler/microservices/udemycurrencyexchangeservice/UdemyCurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package at.hochbichler.microservices.udemycurrencyexchangeservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +12,10 @@ public class UdemyCurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UdemyCurrencyExchangeServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
